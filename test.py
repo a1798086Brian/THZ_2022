9094d86741528456a31bd940ab829e857a8ddf4f
Coding_Tadpole <65011137+a1798086Brian@users.noreply.github.com>
2022-02-06 18:29:34 +1030

    Add files via upload

---
+++ test.py
@@ -0,0 +1,35 @@
+# print("hello world 😺")
+# print("hype " * 10)
+# hello = "hello world 😺"
+
+# variable1 = False
+
+# print(variable1)
+
+
+# news = " heyya whats \\\\ up"
+# print(news)
+
+
+# formatted_string = f"{variable1} {news}"
+# print(formatted_string)
+
+# print(hello.title())
+
+
+# x = input("x: ")
+# print(type(x))
+# y = float(x) + 1
+# print(type(y))
+
+# name = "Bartholomew"
+# print(name[-2:4])
+
+
+count = 0
+for number in range(1, 10):
+    if (number % 2) == 0:
+        print(number)
+        count += 1
+
+print(f"We have {count} even numbers")
